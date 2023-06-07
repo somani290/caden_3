6b3f41a3491f99f7affef6020950296df833c647
somani290 <2020UCP1982@mnit.ac.in>
2023-06-07 08:26:19 +0530

    commit-message

---
+++ project.js
@@ -0,0 +1,71 @@
+function f1(){
+    let a=document.getElementById('imgheart1');
+    a.style.animation="move1 1s"
+     let b=document.getElementById('b1')
+    b.style.borderColor='blue'
+     let d=document.createElement('div')
+     d.id="new"
+    d.innerHTML='<p style="text-align:center;color:black;position:absolute;top:55%"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nam soluta pariatur dolore magni amet voluptas excepturi iste aliquam hic? </p>'
+
+    b.appendChild(d)
+
+}
+
+ function f2(){
+     let a=document.getElementById('imgheart1')
+     a.style.animation="move2 1s"
+     let b=document.getElementById('b1')
+     b.style.borderColor='black'
+     document.querySelector("#new").remove()
+    
+ }
+
+
+
+
+function f3(){
+    let a=document.getElementById('imgheart2');
+    a.style.animation="move1 1s"
+    let b=document.getElementById('b2')
+    b.style.borderColor='blue'
+    let d=document.createElement('div')
+     d.id="new"
+    d.innerHTML='<p style="text-align:center;color:black;position:absolute;top:55%"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nam soluta pariatur dolore magni amet voluptas excepturi iste aliquam hic? </p>'
+
+    b.appendChild(d)
+
+}
+
+function f4(){
+    let a=document.getElementById('imgheart2')
+    a.style.animation="move2 1s"
+    let b=document.getElementById('b2')
+    b.style.borderColor='black'
+    document.querySelector("#new").remove()
+
+}
+
+function f5(){
+    let a=document.getElementById('imgheart3');
+    a.style.animation="move1 1s"
+    let b=document.getElementById('b3')
+    b.style.borderColor='blue'
+    let d=document.createElement('div')
+     d.id="new"
+    d.innerHTML='<p style="text-align:center;color:black;position:absolute;top:55%"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nam soluta pariatur dolore magni amet voluptas excepturi iste aliquam hic? </p>'
+
+    b.appendChild(d)
+
+}
+
+function f6(){
+    let a=document.getElementById('imgheart3')
+    a.style.animation="move2 1s"
+    let b=document.getElementById('b3')
+    b.style.borderColor='black'
+    document.querySelector("#new").remove()
+
+}
+
+
+
